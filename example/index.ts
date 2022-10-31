cfed30ad95672216e6d129c1f32054ed709dd31c
Gaurav Thakur <gaurav2.thakur@ril.com>
2022-10-31 13:30:27 +0530

    update example

--- example/index.ts
+++ example/index.ts
@@ -1,19 +1,19 @@
 import { is, validateSchema } from 'is-validator';
 import type { InferType } from 'is-validator';
 
-const AppSchema = {
+const PersonSchema = {
     name: is.string({ minLength: 2, maxLength: 10 }),
     email: is.string({ isEmail: true, minLength: 5 }),
     age: is.number({ moreThan: 18 }),
 };
 
-type AppProps = InferType<typeof AppSchema>;
+type Person = InferType<typeof PersonSchema>;
 
-const person: AppProps = {
-    name: 'gav',
-    email: 'sdf',
-    age: 8,
+const person: Person = {
+    name: 'A',
+    email: 'gthakur581@gmail.com',
+    age: 10,
 };
 
-const { hasError, errors } = validateSchema(AppSchema, person);
-console.log(errors);
+const result = validateSchema(PersonSchema, person, { shouldThrowError: true });
+console.log(result);
